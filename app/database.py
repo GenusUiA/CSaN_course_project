f35d4deecc776467aac0c62f8af98299df0cf4d8
GenusUiA <kasperetsyevgeny@gmail.com>
2025-08-08 21:44:00 +0300

    work docker-compose

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from os import getenv
 
-DATABASE_URL = "postgresql://postgres:1111@localhost:1111/cloud_storage"
-
+DATABASE_URL = getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
